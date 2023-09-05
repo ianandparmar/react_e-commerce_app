357406f834266595b69b915f3fd417e2a3140ad0
ianandparmar <sameeranandparmar@gmail.com>
2023-09-05 23:51:34 +0530

    styled component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from './SingleProduct';
+import Cart from './Cart';
+import ErrorPage from './ErrorPage';
+import {GlobalStyle} from './GlobalStyle';
+import { ThemeProvider } from "styled-components";
 
 const App = () => {
+  const theme = {
+    colors: {
+      bg: "#000",
+    }
+  };
+
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-    </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/singleproduct/:id" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 };
 
